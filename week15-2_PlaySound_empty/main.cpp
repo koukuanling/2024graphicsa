1e236fbf31fccb3d216351cfb1d42bcec3fb5cfb
ruby <114201536+koukuanling@users.noreply.github.com>
2024-05-30 16:50:14 +0800

    week15

---
+++ week15-2_PlaySound_empty/main.cpp
@@ -0,0 +1,11 @@
+///把程式碼全刪
+#include <windows.h>
+#include <stdio.h>
+int main( )
+{
+    ///PlaySound("C:/Do.wav" , NULL, SND_SYNC);
+    PlaySound("mykbeat.wav" , NULL, SND_ASYNC);
+    printf("請輸入一個整數: ");
+    int n;
+    scanf("%d",&n);
+}
